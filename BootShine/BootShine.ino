3b0f3eb70fa6eeae2a6fade35df279456c897e15
Johok1 <46500902+Johok1@users.noreply.github.com>
2020-11-02 14:26:27 -0600

    init
    added code stuff, more to go

---
+++ BootShine/BootShine.ino
@@ -0,0 +1,57 @@
+int limitSwitchPin = 6;
+//Each Stepper Motor takes 4 pins 
+int rotationalStepperPin1 = 7, rotationalStepperPin2 = 6, rotationalStepperPin3 = 5, rotatioanlStepperPin4 = 4; 
+int extrusionStepperPin1 = 3, extrusionStepperPin2 = 2, extrusionStepperPin3 = 1, extrusionStepperPin4 = 0; 
+int UV_RelayPin = 12; 
+int toggleOnButtonPin = 11; 
+int toggleEmergencyStopPin = 10;
+
+const int stepsPerRevolution = 200; //Depends on stepper motor 
+
+Stepper rotationalStepperMotor(stepsPerRevolution, rotationalStepperPin1, rotationalStepperPin2, rotationalStepperPin3, rotatioanlStepperPin4); 
+Stepper extrusionStepperMotor(stepsPerRevolution, extrusionStepperPin1, extrusionStepperPin2, extrusionStepperPin3, extrusionStepperPin4);
+
+void setup(){
+  pinMode(limitSwitchPin, INPUT); 
+  pinMode(rotationalStepperPin1, INPUT);
+  pinMode(rotationalStepperPin2, INPUT);
+  pinMode(rotatioanlStepperPin3, INPUT); 
+  pinMode(rotatioanlStepperPin4, INPUT); 
+  pinMode(extrusionStepperPin1, INPUT); 
+  pinMode(extrusionStepperPin2, INPUT); 
+  pinMode(extrusionStepperPin3, INPUT); 
+  pinMode(extrusionStepperPin4, INPUT); 
+  pinMode(UV_RelayPin, INPUT); 
+  pinMode(toggleOnButtonPin, INPUT); 
+  pinMode(toggleEmergencyButtonPin, INPUT); 
+}
+
+void loop(){
+  
+}
+
+void move_x_and_rotate(double degree, double mm){
+    Stepper_x.step(mm_to_steps(mm)); 
+    delay(10);
+    digitalWrite(solenoidPin, HIGH); 
+    Stepper_rotation.step(degrees_to_steps(degree));
+
+    delay(10);
+}
+
+void openContainer(){
+  //Move stepper to extrude the table outside of the container for remote operation. 
+  Stepper_x.step(mm_to_steps(mm));
+}
+void closeContainer(){
+  //Move stepper to extrude the table outside of the container for remote operation. 
+  Stepper_x.step(mm_to_steps(-mm));
+}
+
+double mm_to_steps(double mm){
+  //TODO: Convert mm to steps
+}
+
+double degrees_to_steps(double degree){
+  //TODO: Convert degrees to steps 
+}
